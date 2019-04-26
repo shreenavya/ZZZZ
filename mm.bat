2e09edee448d3f6c6131b692c54d15e37eb1365e
shreenavya <managerm911@gmail.com>
2019-04-26 00:14:03 -0700

    zzzzz

---
+++ mm.bat
@@ -0,0 +1,2 @@
+set classpath=D:\Photon\ZZZZZ\lib\*;D:\Photon\ZZZZZ\bin
+java org.testng.TestNG testng.xml
---
+++ src/ZZ/Z.java
@@ -0,0 +1,11 @@
+package ZZ;
+
+import org.testng.annotations.Test;
+
+public class Z {
+	@Test
+	public void t3()
+	{
+System.out.println("hidkajlkahzlk");		
+	}
+}
